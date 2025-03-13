79935b036707b9d4d5339a2494162a4741d42455
maulidaprina <mldaprna@gmail.com>
2025-03-13 14:29:46 +0700

    modifikasi faktorial.java

--- BruteForceDivideConquer/src/minggu5/Faktorial.java
+++ BruteForceDivideConquer/src/minggu5/Faktorial.java
@@ -5,14 +5,16 @@ import java.util.Scanner;
 public class Faktorial {
     int nilai;
 
-public int faktorialBF (int n) {
-    int fakto = 1;
-        for (int i = 1; i <= n; i++) {
-            fakto = fakto * i;
-     }
+    public int faktorialBF (int n) {
+        int fakto = 1;
+        int i = 1;
+        while (i <= n) {
+            fakto *= i;
+            i++;
+        }
         return fakto;
     }
-
+    
     int faktorialDC (int n) {
         if (n == 1) {
             return 1;
